a65ece4ea16831fb57a1f129666f7ce18479fd2f
Ankeraout <ankeraout@yahoo.com>
2019-07-03 21:10:42 +0200

    Added javadoc

--- src/fr/ankeraout/mcank/worldgen/FlatgrassWorldGenerator.java
+++ src/fr/ankeraout/mcank/worldgen/FlatgrassWorldGenerator.java
@@ -2,40 +2,48 @@ package fr.ankeraout.mcank.worldgen;
 
 import fr.ankeraout.mcank.Blocks;
 
+/**
+ * This class is a world generator that will generate flat grass maps. The
+ * layers from 0 to (worldHeight / 2) - 1 will be made of dirt, and the surface
+ * layer at height (worldHeight / 2) will be made of grass.
+ * 
+ * @author Ankeraout
+ *
+ */
 public class FlatgrassWorldGenerator extends WorldGenerator {
 	@Override
 	public void generateChunk(int[] worldData, int worldWidth, int worldHeight, int worldDepth, int chunkX, int chunkZ,
 			long seed) {
 		int chunkBlockX = chunkX * 16;
 		int chunkBlockZ = chunkZ * 16;
-		
-		for(int y = 0; y < (worldHeight / 2) - 1; y++) {
-			for(int x = chunkBlockX; x < chunkBlockX + 16; x++) {
-				for(int z = chunkBlockZ; z < chunkBlockZ + 16; z++) {
+
+		for (int y = 0; y < (worldHeight / 2) - 1; y++) {
+			for (int x = chunkBlockX; x < chunkBlockX + 16; x++) {
+				for (int z = chunkBlockZ; z < chunkBlockZ + 16; z++) {
 					worldData[(y * worldDepth + z) * worldWidth + x] = Blocks.DIRT.getBlockId();
 				}
 			}
 		}
-		
-		for(int x = chunkBlockX; x < chunkBlockX + 16; x++) {
-			for(int z = chunkBlockZ; z < chunkBlockZ + 16; z++) {
+
+		for (int x = chunkBlockX; x < chunkBlockX + 16; x++) {
+			for (int z = chunkBlockZ; z < chunkBlockZ + 16; z++) {
 				worldData[((worldHeight / 2) * worldDepth + z) * worldWidth + x] = Blocks.DIRT.getBlockId();
 			}
 		}
 	}
-	
+
 	@Override
 	public void generateWorld(int[] worldData, int worldWidth, int worldHeight, int worldDepth, long seed) {
-		for(int y = 0; y < (worldHeight / 2) - 1; y++) {
-			for(int x = 0; x < worldWidth; x++) {
-				for(int z = 0; z < worldDepth; z++) {
+		for (int y = 0; y < (worldHeight / 2) - 1; y++) {
+			for (int x = 0; x < worldWidth; x++) {
+				for (int z = 0; z < worldDepth; z++) {
 					worldData[(y * worldDepth + z) * worldWidth + x] = Blocks.DIRT.getBlockId();
 				}
 			}
 		}
-		
-		for(int x = 0; x < worldWidth; x++) {
-			for(int z = 0; z < worldDepth; z++) {
+
+		for (int x = 0; x < worldWidth; x++) {
+			for (int z = 0; z < worldDepth; z++) {
 				worldData[((worldHeight / 2) * worldDepth + z) * worldWidth + x] = Blocks.GRASS.getBlockId();
 			}
 		}
--- src/fr/ankeraout/mcank/worldgen/WorldGeneratorFactory.java
+++ src/fr/ankeraout/mcank/worldgen/WorldGeneratorFactory.java
@@ -4,17 +4,41 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * This class contains a list of all the available world generators. It is a
+ * singleton factory, which means that it can be accessed from anywhere in the
+ * code.
+ * 
+ * @author Ankeraout
+ *
+ */
 public class WorldGeneratorFactory {
+	/**
+	 * The unique instance of this class (singleton).
+	 */
 	private static WorldGeneratorFactory instance;
-	
+
+	/**
+	 * The list of all the available world generators.
+	 */
 	private HashMap<String, WorldGenerator> worldGenerators;
 
+	/**
+	 * This constructor is made private in order to prevent external instantiation
+	 * because this class is a singleton. It initializes the list of world
+	 * generators with the default ones.
+	 */
 	private WorldGeneratorFactory() {
 		// Register the default map generators
 		this.worldGenerators = new HashMap<String, WorldGenerator>();
 		this.worldGenerators.put("flatgrass", new FlatgrassWorldGenerator());
 	}
 
+	/**
+	 * Returns the unique instance of the {@link WorldGeneratorFactory} class.
+	 * 
+	 * @return The unique instance of the {@link WorldGeneratorFactory} class.
+	 */
 	public static synchronized WorldGeneratorFactory getInstance() {
 		if (WorldGeneratorFactory.instance == null) {
 			WorldGeneratorFactory.instance = new WorldGeneratorFactory();
@@ -23,18 +47,43 @@ public class WorldGeneratorFactory {
 		return WorldGeneratorFactory.instance;
 	}
 
+	/**
+	 * Returns the generator from its name. If no generator was previously
+	 * registered with the given name, then this method will return
+	 * <code>null</code>.
+	 * 
+	 * @param generatorName The name of the generator. (ex: flatgrass)
+	 * @return The generator that was registered with the given name.
+	 */
 	public WorldGenerator getGenerator(String generatorName) {
 		return this.worldGenerators.get(generatorName);
 	}
-	
+
+	/**
+	 * Registers a new generator on the generator list. If another generator was
+	 * already previously registered with the same generator name, then this method
+	 * will throw an {@link IllegalArgumentException}.
+	 * 
+	 * @param generatorName The name of the generator
+	 * @param generator     The {@link WorldGenerator} object that will be returned
+	 *                      by {@link WorldGeneratorFactory#getGenerator(String)}
+	 *                      when getting the generator with the given name.
+	 * @throws IllegalArgumentException If another generator was already previously
+	 *                                  registered with the same generator name.
+	 */
 	public void addGenerator(String generatorName, WorldGenerator generator) {
-		if(this.worldGenerators.containsKey(generatorName)) {
-			throw new IllegalArgumentException("A world generator with the name \"" + generatorName + "\" is already registered.");
+		if (this.worldGenerators.containsKey(generatorName)) {
+			throw new IllegalArgumentException(
+					"A world generator with the name \"" + generatorName + "\" is already registered.");
 		}
-		
+
 		this.worldGenerators.put(generatorName, generator);
 	}
-	
+
+	/**
+	 * Returns a set that contains the names of all the available generators.
+	 * @return A set that contains the names of all the available generators.
+	 */
 	public Set<String> getAvailableGenerators() {
 		return new HashSet<String>(this.worldGenerators.keySet());
 	}
